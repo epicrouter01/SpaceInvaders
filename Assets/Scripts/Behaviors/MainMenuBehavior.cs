8ccb8a2e87c7d09d31cfe547d016de2227bd102d
Epic Router <epicrouter01@gmail.com>
2021-06-02 20:15:03 +0300

    added main scene

---
+++ Assets/Scripts/Behaviors/MainMenuBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBehavior : MonoBehaviour
+{
+    public void play()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
